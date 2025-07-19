8541b305a0e1aa0b9ed5a29c03fe1ad2e721bfc8
harsh kaptan <161920954+harshkaptan@users.noreply.github.com>
2025-07-19 14:38:24 +0530

    Time: 0 ms (100%), Space: 53.9 MB (84.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,9 +1,8 @@
-const twoSum = function(nums, target) {
+const twoSum = (nums, target) => {
+    const map = {};
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
-        }
+        const diff = target - nums[i];
+        if (diff in map) return [map[diff], i];
+        map[nums[i]] = i;
     }
 };
